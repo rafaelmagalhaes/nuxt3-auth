c2567bf5b384ab1ecc995308e422b759cc2713e5
Rafael Magalhaes <rafael.magalhaes@yapily.com>
2022-12-14 19:03:34 +0000

    deploy on gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  router: {
+    base: '/nuxt3-auth/',
+  },
   modules: [
     // ...
     [
